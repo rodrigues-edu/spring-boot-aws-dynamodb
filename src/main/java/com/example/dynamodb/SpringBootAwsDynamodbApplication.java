12994ea14aa656dde6e0a43141ac50b4f17fcd20
Eduardo Rodrigues <silva.rodriguesedu@icloud.com>
2022-09-26 11:19:55 -0300

    Criação do Projeto

---
+++ src/main/java/com/example/dynamodb/SpringBootAwsDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsDynamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAwsDynamodbApplication.class, args);
+    }
+
+}
